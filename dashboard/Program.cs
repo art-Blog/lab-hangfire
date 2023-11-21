c81af3dcba8adfcc7b4ea7f9e175a91436dd47f5
art <partypeopleland@gmail.com>
2023-11-21 16:39:28 +0800

    add dashboard metrics

--- dashboard/Program.cs
+++ dashboard/Program.cs
@@ -7,7 +7,22 @@ var redisConnectionString = builder.Configuration.GetConnectionString("Redis");
 
 // Add services to the container.
 builder.Services.AddRazorPages();
-builder.Services.AddHangfire(x => x.UseRedisStorage(redisConnectionString));
+builder.Services.AddHangfire(config =>
+{
+    config.UseRedisStorage(redisConnectionString);
+    config.UseDashboardMetric(Hangfire.Dashboard.DashboardMetrics.ServerCount) //服务器数量
+        .UseDashboardMetric(Hangfire.Dashboard.DashboardMetrics.RecurringJobCount) //任务数量
+        .UseDashboardMetric(Hangfire.Dashboard.DashboardMetrics.RetriesCount) //重试次数
+        //.UseDashboardMetric(Hangfire.Dashboard.DashboardMetrics.EnqueuedCountOrNull)//队列数量
+        //.UseDashboardMetric(Hangfire.Dashboard.DashboardMetrics.FailedCountOrNull)//失败数量
+        .UseDashboardMetric(Hangfire.Dashboard.DashboardMetrics.EnqueuedAndQueueCount) //队列数量
+        .UseDashboardMetric(Hangfire.Dashboard.DashboardMetrics.ScheduledCount) //计划任务数量
+        .UseDashboardMetric(Hangfire.Dashboard.DashboardMetrics.ProcessingCount) //执行中的任务数量
+        .UseDashboardMetric(Hangfire.Dashboard.DashboardMetrics.SucceededCount) //成功作业数量
+        .UseDashboardMetric(Hangfire.Dashboard.DashboardMetrics.FailedCount) //失败数量
+        .UseDashboardMetric(Hangfire.Dashboard.DashboardMetrics.DeletedCount) //删除数量
+        .UseDashboardMetric(Hangfire.Dashboard.DashboardMetrics.AwaitingCount); //等待任务数量
+});
 
 var app = builder.Build();
 
